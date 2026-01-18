b67909b1a2362d0829a032439efd5e9b2791128f
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2026-01-18 14:24:22 -0800

    Add better error messages

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -1,21 +1,19 @@
 use clap::{Parser};
+use anyhow::{Context, Result};
 
 #[derive(Parser)]
 struct Cli {
     cmd: String,
     path: std::path::PathBuf,
 }
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>>{
     let args = Cli::parse();
-    let result = std::fs::read_to_string(&args.path);
-    let content = match result {
-        Ok(content) => {content},
-        Err(error) => {panic!("bad file: {}", error)}
-    };
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
     println!("IT WORKS! cmd: {:?} path: {:?}", args.cmd, args.path);
 
     for line in content.lines() {
         println!("{}", line);
     }
-
+    Ok(())
 }
