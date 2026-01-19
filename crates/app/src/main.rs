96fe9c62e2b41772a0d3332efeed9aae286351b8
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2026-01-18 17:36:32 -0800

    Use subcommands

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -1,19 +1,42 @@
-use clap::{Parser};
+use clap::{Args, Parser, Subcommand};
 use anyhow::{Context, Result};
+// use midiedit_cli;
 
+/// A Simple Midi Editor
 #[derive(Parser)]
+#[command(version, about, long_about = None)]
 struct Cli {
-    cmd: String,
-    path: std::path::PathBuf,
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// reads and prints the file
+    Read(ReadFile),
+}
+
+#[derive(Args)]
+struct ReadFile {
+    /// path of file to read
+    file: std::path::PathBuf
 }
 fn main() -> Result<(), Box<dyn std::error::Error>>{
-    let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path)
-        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
-    println!("IT WORKS! cmd: {:?} path: {:?}", args.cmd, args.path);
+    // midiedit_cli::test();
+    let cli = Cli::parse();
+    
+    match &cli.command {
+        Commands::Read(read_file) => {
+            let content = std::fs::read_to_string(&read_file.file)
+                .with_context(|| format!("could not read file `{}`", read_file.file.display()))?;
 
-    for line in content.lines() {
-        println!("{}", line);
+            println!("IT WORKS! path: {:?}", read_file.file);
+
+            for line in content.lines() {
+                println!("{}", line);
+            }
+        }
     }
+
     Ok(())
 }
