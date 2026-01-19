66a6e8c2d20d102c4b03203a7cea1b795a9fb156
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2026-01-18 21:35:34 -0800

    Add note detection

--- crates/cli/src/lib.rs
+++ crates/cli/src/lib.rs
@@ -1,4 +1,4 @@
-use midly::{Smf, TrackEventKind, MetaMessage};
+use midly::{MetaMessage, MidiMessage, Smf, TrackEventKind};
 use anyhow::{Context, Ok, Result};
 
 pub fn test() {
@@ -15,21 +15,27 @@ pub fn read_file(path: std::path::PathBuf) -> Result<()>{
     let data = read_midi_file(&path)?;
     println!("{:?} exists!", path);
 
-    let smf = midly::Smf::parse(&data)
+    let smf = Smf::parse(&data)
         .with_context(|| format!("could not parse file"))?;
 
     println!("{} tracks", smf.tracks.len());
 
     let mut tempo = 120;
+    let mut found_tempo = false;
 
     for track in smf.tracks {
         for event in track {
-            if let TrackEventKind::Meta(msg) = event.kind {
+            if !found_tempo && let TrackEventKind::Meta(msg) = event.kind {
                 if let MetaMessage::Tempo(tempo_meta) = msg {
                     let ms_tempo = tempo_meta.as_int(); // Tempo is represented in microseconds/quarter note
                     let real_tempo = 60000000 / ms_tempo;
                     tempo = real_tempo;
+                    found_tempo = true;
                     break;
+                } 
+            } else if let TrackEventKind::Midi { channel: _, message: msg } = event.kind {
+                if let MidiMessage::NoteOn { key, vel: _ } = msg {
+                    println!("Key {:?} played", key)
                 }
             }
         }
