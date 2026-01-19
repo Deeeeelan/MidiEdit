cdea6ad3c3c8e35a3ea887a65180bd53a31ed6a8
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2026-01-18 21:00:07 -0800

    Add tempo detection

--- crates/cli/src/lib.rs
+++ crates/cli/src/lib.rs
@@ -1,4 +1,4 @@
-use midly;
+use midly::{Smf, TrackEventKind, MetaMessage};
 use anyhow::{Context, Ok, Result};
 
 pub fn test() {
@@ -12,14 +12,30 @@ fn read_midi_file(path: &std::path::PathBuf) -> Result<Vec<u8>> {
 }
 
 pub fn read_file(path: std::path::PathBuf) -> Result<()>{
-
-    
     let data = read_midi_file(&path)?;
     println!("{:?} exists!", path);
 
-    let smf = midly::Smf::parse(&data).unwrap();
+    let smf = midly::Smf::parse(&data)
+        .with_context(|| format!("could not parse file"))?;
 
     println!("{} tracks", smf.tracks.len());
+
+    let mut tempo = 120;
+
+    for track in smf.tracks {
+        for event in track {
+            if let TrackEventKind::Meta(msg) = event.kind {
+                if let MetaMessage::Tempo(tempo_meta) = msg {
+                    let ms_tempo = tempo_meta.as_int(); // Tempo is represented in microseconds/quarter note
+                    let real_tempo = 60000000 / ms_tempo;
+                    tempo = real_tempo;
+                    break;
+                }
+            }
+        }
+    }
+
+    println!("The starting tempo of the track is: {}", tempo);
     
     Ok(())
 }
