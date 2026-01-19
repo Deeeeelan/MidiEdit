e3f5b2d07612d99bc1d79abe30fd3f97573c9a63
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2026-01-19 11:40:13 -0800

    Refactor midi parsing into separate files

--- crates/edit-engine/src/lib.rs
+++ crates/edit-engine/src/lib.rs
@@ -1,20 +1,19 @@
 use midly::{MidiMessage, Smf, TrackEventKind, num::u7};
 use anyhow::{Context, Ok, Result};
 
-fn read_midi_file(path: &std::path::PathBuf) -> Result<Vec<u8>> {
+fn read_file(path: &std::path::PathBuf) -> Result<Vec<u8>> {
     let data = std::fs::read(path)
         .with_context(|| format!("could not read file `{}`", path.display()))?;
     Ok(data)
 }
 
-pub fn transpose(path: std::path::PathBuf, distance: i8, start: i32, end: i32) -> Result<()>{
-    let data = read_midi_file(&path)?;
+fn parse_midi_file(data: &[u8]) -> Result<Smf<'static>> {
+    let smf = Smf::parse(&data)
+        .with_context(|| format!("could not parse file"))?.make_static();
+    Ok(smf)
+}
 
-    let mut smf = Smf::parse(&data)
-        .with_context(|| format!("could not parse file"))?;
-
-    println!("Transposing with distance {:?}", distance);
-
+fn transpose_smf_region(smf: &mut Smf, distance: i8, start: i32, end: i32) {
     for track in &mut smf.tracks {
         for event in track {
             if let TrackEventKind::Midi { channel: _, message: msg } = &mut event.kind {
@@ -26,9 +25,16 @@ pub fn transpose(path: std::path::PathBuf, distance: i8, start: i32, end: i32) -
             }
         }
     }
+}
+
+pub fn transpose(path: std::path::PathBuf, distance: i8, start: i32, end: i32) -> Result<()>{
+    let data = read_file(&path)?;
+    let mut smf = parse_midi_file(&data)?;
+
+    println!("Transposing by {:?}", distance);
+    transpose_smf_region(&mut smf, distance, start, end);
 
     smf.save(&path)?;
-
     
     Ok(())
 }
